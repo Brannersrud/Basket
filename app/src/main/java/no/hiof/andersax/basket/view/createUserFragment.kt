6368eab32bfe9ee73d5161f99c060f2de0d7887f
Anders Saxrud <brannersrud@gmail.com>
2019-09-04 20:45:08 +0200

    Added styles for login and create user. Added Junit library and navgraph/navigation

---
+++ app/src/main/java/no/hiof/andersax/basket/view/createUserFragment.kt
@@ -0,0 +1,25 @@
+package no.hiof.andersax.basket.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import no.hiof.andersax.basket.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class createUserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_user, container, false)
+    }
+
+
+}
