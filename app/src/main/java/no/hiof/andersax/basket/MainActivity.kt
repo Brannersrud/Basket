6215dd3f5d6afaf2956c43a2f081af64a476e1f8
Anders Saxrud <brannersrud@gmail.com>
2019-11-04 19:12:21 +0100

    finally fixed the avatar issue

--- app/src/main/java/no/hiof/andersax/basket/MainActivity.kt
+++ app/src/main/java/no/hiof/andersax/basket/MainActivity.kt
@@ -1,17 +1,42 @@
 package no.hiof.andersax.basket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.mytoolbar))
 
 
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var id : Int = item.itemId
+
+        if(id == R.id.action_favorite){
+            val myIntent = Intent(this, profileActivity::class.java)
+            startActivity(myIntent)
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
