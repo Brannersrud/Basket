f98e310dcfcca04f8531016ff7156720a553cf54
Anders Saxrud <brannersrud@gmail.com>
2019-11-21 23:59:59 +0100

    landscape fix

--- app/src/main/java/no/hiof/andersax/basket/view/listOverviewFragment.kt
+++ app/src/main/java/no/hiof/andersax/basket/view/listOverviewFragment.kt
@@ -3,6 +3,7 @@ package no.hiof.andersax.basket.view
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -39,7 +40,9 @@ class listOverviewFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         (activity as AppCompatActivity).supportActionBar!!.show()
+
 
 
     }
